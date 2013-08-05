64cb1a81406082a611ffc55ca295406f466f76ae
Albert Wang <albertyw@mit.edu>
2013-08-05 19:01:24 -0400

    Added skeleton server

--- server/base.py
+++ server/base.py
@@ -1,9 +1,20 @@
+import json
+
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Localisation 2.0"
+
+@app.route("/data/", methods=['GET','POST'])
+def data():
+    if 'data' not in request.form:
+        return 'Nothing received'
+    data = json.loads(request.form['data'])
+    return str(data)
 
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0')
---
+++ server/run_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python base.py
+sleep 2s
+./run_server.sh
