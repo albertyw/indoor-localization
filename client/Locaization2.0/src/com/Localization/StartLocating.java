40bec6728983311eb708b9f91d0d2cdc68574f2c
austindcollins <austindcollins@gmail.com>
2013-08-05 18:39:31 -0700

    Added hello world android app, updated gitignore

---
+++ client/Locaization2.0/src/com/Localization/StartLocating.java
@@ -0,0 +1,23 @@
+package com.Localization;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartLocating extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_locating);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start_locating, menu);
+        return true;
+    }
+    
+}
