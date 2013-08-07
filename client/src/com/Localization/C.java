541bc605cd992b9ac317b3767a159e3846cf7fa1
Szymon Sidor <szymon.sidor@gmail.com>
2013-08-06 18:56:55 -0700

    Pushing data continously by start/stop button. Wifi pushed to server in a nice format. Easy to extend with new functions. Data can be null and the it gets ignored by addData function.

--- client/src/com/Localization/C.java
+++ client/src/com/Localization/C.java
@@ -2,6 +2,7 @@ package com.Localization;
 
 public class C {
 	public static final String TAG = "local2";
+	public static final long pushIntervalMillis = 200;
     // Szymon
     public static final String SERVER = "http://192.168.3.61/";
     // Albert
--- client/src/com/Localization/StartLocating.java
+++ client/src/com/Localization/StartLocating.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -23,71 +24,114 @@ import org.json.simple.JSONValue;
 // See http://www.androidsnippets.com/scan-for-wireless-networks
 
 public class StartLocating extends Activity {
-    WifiManager mainWifi;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_locating);
-        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        Button b = (Button)findViewById(R.id.ping_server);
-        ErrorReporting.initialize(this);
-        
-        b.setOnClickListener(new OnClickListener() {
-			
+	WifiManager mainWifi;
+
+	Handler dataPushHandler;
+	boolean dataPushHandlerActive;
+	Button btnStartPushing;
+	Button btnStopPushing;
+
+	Runnable statusChecker = new Runnable() {
+		@Override 
+		public void run() {
+			List locationData = getAvailableData();
+			String jsonStringified = JSONValue.toJSONString(locationData);
+
+			Log.d(C.TAG, "JSON encoded data: " + jsonStringified.substring(0, 100) + (jsonStringified.length() > 100 ? "..." : ""));
+			Networking.postData(C.SERVER + "push", jsonStringified);
+			if (dataPushHandlerActive)
+				dataPushHandler.postDelayed(this, C.pushIntervalMillis);
+		}
+	};
+	
+	private void startPushing() {
+		dataPushHandlerActive = true;
+		statusChecker.run();
+		btnStopPushing.setEnabled(true);
+		btnStartPushing.setEnabled(false);
+	}
+	
+	private void stopPushing() {
+		dataPushHandlerActive = false;
+		dataPushHandler.removeCallbacks(statusChecker);
+		btnStopPushing.setEnabled(false);
+		btnStartPushing.setEnabled(true);
+	}
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_locating);
+		mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		dataPushHandler = new Handler();
+
+		dataPushHandlerActive = false;
+		ErrorReporting.initialize(this);
+
+		btnStartPushing = (Button)findViewById(R.id.start_push);
+		btnStopPushing = (Button)findViewById(R.id.stop_pushing);
+		btnStopPushing.setEnabled(false);
+		btnStartPushing.setEnabled(true);
+		
+		btnStartPushing.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				List locationData = getAvailableData();
-                String jsonStringified = JSONValue.toJSONString(locationData);
-		        
-		        Log.d(C.TAG, "JSON encoded data: " + jsonStringified);
-				Networking.postData(C.SERVER + "push", jsonStringified);
+				startPushing();
 			}
 		});
-        
-    }
-    // Adds data to target if data is not null and denotes it as name
-    private void addData(List target, String name, Object data) {
-    	if (data != null) {
-    		Map dataContainer = new HashMap();
-    		dataContainer.put("name", name);
-    		dataContainer.put("data", data);
-    		target.add(dataContainer);
-    	}
-    }
-    
-    protected List getAvailableData() {
-    	Long start = System.currentTimeMillis();
-    	
-    	List result = new LinkedList();
-    	
-    	// Wifi
-    	addData(result, "wifi", getWifi());
-    	
-    	Log.d(C.TAG, "Data extraction complete in " + (System.currentTimeMillis() - start) + " ms");
-    	return result;
-    }
+		
+		btnStopPushing.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stopPushing();
+			}
+		});
 
-    protected List getWifi() {
-    	mainWifi.startScan();
-    	List jsonScanResults = new LinkedList();
-        
-        for(ScanResult scanResult : mainWifi.getScanResults()) {
-            HashMap jsonScanResult = new HashMap();
-            jsonScanResult.put("bssid", scanResult.BSSID);
-            jsonScanResult.put("level", scanResult.level);
-            jsonScanResult.put("frequency", scanResult.frequency);
-            jsonScanResults.add(jsonScanResult);
-        }
-        
-        return jsonScanResults;
-    }
+	}
+	// Adds data to target if data is not null and denotes it as name
+	private void addData(List target, String name, Object data) {
+		if (data != null) {
+			Map dataContainer = new HashMap();
+			dataContainer.put("name", name);
+			dataContainer.put("data", data);
+			target.add(dataContainer);
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start_locating, menu);
-        return true;
-    }
+	protected List getAvailableData() {
+		Long start = System.currentTimeMillis();
+
+		List result = new LinkedList();
+
+		// Wifi
+		addData(result, "wifi", getWifi());
+
+		Log.d(C.TAG, "Data extraction complete in " + (System.currentTimeMillis() - start) + " ms");
+		return result;
+	}
+
+	protected List getWifi() {
+		mainWifi.startScan();
+		List jsonScanResults = new LinkedList();
+
+		for(ScanResult scanResult : mainWifi.getScanResults()) {
+			HashMap jsonScanResult = new HashMap();
+			jsonScanResult.put("bssid", scanResult.BSSID);
+			jsonScanResult.put("level", scanResult.level);
+			jsonScanResult.put("frequency", scanResult.frequency);
+			jsonScanResults.add(jsonScanResult);
+		}
+
+		return jsonScanResults;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_locating, menu);
+		return true;
+	}
 
 }
