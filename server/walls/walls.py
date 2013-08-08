b1fcb0379b7e1fb8ab0cf8bed38bfb67ebc804eb
pjamaral <pjamaral@berkeley.edu>
2013-08-08 15:51:49 -0700

    made the walls frozen set smaller

--- server/walls/walls.py
+++ server/walls/walls.py
@@ -1,16 +1,15 @@
 from PIL import Image
 import pickle
 
-img = Image.open('edited_map.png')
-print img
-(minX, maxX) = (5800, 0)
-(minY, maxY) = (780, 0)
+img = Image.open('skinny.png')
 wall_list = []
+count = 0
 for x in range(5800):
     for y in range(780):
         pixel = img.getpixel((x,y))
         if pixel[0] - pixel[1] > 100:
             wall_list.append((x,y))
-
+            count += 1
+print count
 with open('walls.p', 'w') as outfile:
   pickle.dump(frozenset(wall_list), outfile)
